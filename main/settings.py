e9cdf691e35a3a432d445951ebe195493b2965c2
Mbutler1991 <147847873+Mbutler1991@users.noreply.github.com>
2024-01-19 19:30:14 +0000

    added .env and updated settings.py

--- main/settings.py
+++ main/settings.py
@@ -18,11 +18,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 if os.path.exists('env.py'):
     import env
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
