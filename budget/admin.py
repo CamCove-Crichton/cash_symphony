99dddb51882829ed2f69e12f4df767f42f2f2b42
Reiko Yazaki <reikoyazaki@Reikos-MacBook-Air.local>
2024-01-20 22:22:48 +0900

    customize Expense model in admin panel

--- budget/admin.py
+++ budget/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
+"""This module holds classes to customize the admin panel."""
 
-# Register your models here.
+from django.contrib import admin
+from .models import Expense
+
+
+@admin.register(Expense)
+class PostAdmin(admin.ModelAdmin):
+    """
+    Customize the list, search and filter items of Post model
+    on the admin panel.
+    """
+    list_display = ('user', 'name', 'amount', 'budget_month')
+    list_filter = ('user', 'budget_month', 'type')
