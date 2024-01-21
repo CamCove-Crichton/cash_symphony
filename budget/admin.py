5fde84c3363f9e96fe0aa5236a14719ab961dfe3
Reiko Yazaki <reikoyazaki@Reikos-MacBook-Air.local>
2024-01-21 09:06:01 +0900

    comment out budget/admin.py

--- budget/admin.py
+++ budget/admin.py
@@ -6,10 +6,10 @@ from .models import Expense
 """
 @admin.register(Expense)
 class PostAdmin(admin.ModelAdmin):
-    
+
     Customize the list, search and filter items of Expense model
     on the admin panel.
-    
+
     list_display = ('user', 'name', 'amount', 'budget_month')
     list_filter = ('user', 'budget_month', 'type')
 """
--- main/settings.py
+++ main/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = [os.environ.get("HOST"), 'localhost']
+ALLOWED_HOSTS = [os.environ.get("HOST"), '127.0.0.1']
 
 
 # Application definition
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
+
     #All Auth
     'django.contrib.sites',
     'allauth',
